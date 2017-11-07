b431e271a03ef3e729644254db8c652da91399b5
NightjarNZ <nightjarnz@gmail.com>
2017-11-08 00:12:18 +1300

    WIP: making a mess because it's the middle of the night & YOLO

--- client/src/components/Meatballs.js
+++ client/src/components/Meatballs.js
@@ -1,10 +1,33 @@
-import jQuery from 'jquery';
 import { inject } from 'lib/Injector';
+import React, { PropTypes } from 'react';
 
-jQuery.entwine('ss', $ => {
-  $('.ss-gridfield .meatball-menu__container').entwine({
-    onmatch() {
-
-    }
-  })
+const Meatballs = ({ PopoverField, id, children }) => ({
+  render() {
+    return (
+      <PopoverField
+        id={id}
+        buttonSize="sm"
+        data={{ placement: 'bottom' }}
+      >
+        {children}
+      </PopoverField>
+    );
+  }
 });
+
+Meatballs.propTypes = {
+  id: PropTypes.string.isRequired,
+  PopoverField: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+};
+
+Meatballs.defaultProps = {
+  id: '',
+  actions: [],
+  PopoverField: null,
+};
+
+export default inject(
+  ['PopoverField'],
+  (PopoverField) => ({ PopoverField }),
+  () => 'Meatballs'
+)(Meatballs);
--- client/src/components/MeatballsMenu.js
+++ client/src/components/MeatballsMenu.js
@@ -2,21 +2,36 @@ import jQuery from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { DropdownButton, MenuItem } from 'react-bootstrap-ss';
+import { provideInjector } from 'lib/Injector';
+import Meatballs from 'components/Meatballs';
+
+const InjectedMeatballs = provideInjector(Meatballs);
 
 jQuery.entwine('ss', $ => {
   $('.ss-gridfield .meatball-menu__container').entwine({
     onmatch() {
       const itemId = this.closest('.ss-gridfield-item').data('id');
       ReactDOM.render(
+        <InjectedMeatballs id={`meatball-menu_${itemId}`}>
+          <MenuItem>Hard coded values for now</MenuItem>
+          <MenuItem divider />
+          <MenuItem href={'no'}>No.</MenuItem>
+        </InjectedMeatballs>,
+        this.get(0),
+      );
+    },
+    something() {
+      const itemId = this.closest('.ss-gridfield-item').data('id');
+      ReactDOM.render(
         <DropdownButton
           id={`meatball-menu_${itemId}`}
           title=""
           className="font-icon-dot-3"
-          noCaret={1}
-          pullRight={true}
-          bsStyle='secondary'
-          bsSize='sm'
-          rootCloseEvent='mousedown'
+          noCaret
+          pullRight
+          bsStyle={'secondary'}
+          bsSize={'sm'}
+          rootCloseEvent={'mousedown'}
         >
           <MenuItem>Hard coded values for now</MenuItem>
           <MenuItem divider />
