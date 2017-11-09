29727dfd878c76e6182c8c026bf3a4295d0684b3
Dylan Wagstaff <dylan@silverstripe.com>
2017-11-09 13:03:18 +1300

    clean up final state from all WIP noise (ie. run prettier)

--- client/src/components/MeatballsMenu.js
+++ client/src/components/MeatballsMenu.js
@@ -19,36 +19,30 @@ jQuery.entwine('ss', $ => {
       return this.closest('.ss-gridfield-item').data('id');
     },
     getData() {
-      return JSON.parse(
-        decodeURIComponent(this.data('actions')) || '[]'
-      );
+      return JSON.parse(decodeURIComponent(this.data('actions')) || '[]');
     },
     drawMeatballs() {
       const items = [];
-      this.getData().forEach(
-        (v, i, a) => {
-          if (items.length) {
-            items.push(
-              <div className="dropdown-divider" />
-            );
-          }
-          items.push(v.map(
-            ({ Title, Link, Type }) => (
-              <a
-                href={Link}
-                className={`dropdown-item meatball-menu__${Type}-action`}
-              >
-                {Title}
-              </a>
-            )
-          ));
+      this.getData().forEach((menuGroup) => {
+        if (items.length) {
+          items.push(<div className="dropdown-divider" />);
         }
-      );
+        items.push(
+          menuGroup.map(({ Title, Link, Type }) => (
+            <a href={Link} className={`dropdown-item meatball-menu__${Type}-action`}>
+              {Title}
+            </a>
+          )),
+        );
+      });
+      // The CMS <body> doesn't scroll as the Popover component expects,
+      // so we need to position relative to the thing that does, to keep it aligned.
+      // (and also assure it is positioned appropriately to support this)
+      const scrollingContainer = this.closest('form')
+        .css('position', 'relative')
+        .get(0);
       ReactDOM.render(
-        <InjectedMeatballs
-          id={`meatball-menu_${this.getItemId()}`}
-          container={this.closest('form').css('position', 'relative').get(0)}
-        >
+        <InjectedMeatballs id={`meatball-menu_${this.getItemId()}`} container={scrollingContainer}>
           {items}
         </InjectedMeatballs>,
         this.get(0),
@@ -69,7 +63,7 @@ jQuery.entwine('ss', $ => {
           const newContentEls = $('.cms-container').handleAjaxResponse(data, status, xhr);
           if (!newContentEls) return;
           newContentEls.filter('form').trigger('aftersubmitform', { status, xhr });
-        }
+        },
       });
       e.stopPropagation();
       return false;
